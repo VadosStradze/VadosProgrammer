28da91f4a64f129779c00699c03c7e7281b2a9f8
VadosStradze <Vados.Stradze@gmail.com>
2020-02-24 23:56:29 +0300

    Add class People

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main  {
+public class Main extends People {
 
     public static void main(String[] args) {
 
---
+++ src/com/company/People.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class People {
+    private String Login;
+    private Integer Password;
+
+
+
+    void info (){
+        System.out.println("User information:");
+        System.out.println("User login: " + Login +  "/n User Password " + Password);
+
+
+
+    }
+
+    public String getLogin() {
+        return Login;
+    }
+
+    public Integer getPassword() {
+        return Password;
+    }
+
+    public void setLogin(String login) {
+        Login = login;
+    }
+
+    public void setPassword(Integer password) {
+        this.Password = password;
+    }
+}
