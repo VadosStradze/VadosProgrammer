990963ac38ba0d57121405efb0dc3e784eceaa1d
VadosStradze <Vados.Stradze@gmail.com>
2020-02-24 21:57:24 +0300

    Commit 2

---
+++ src/People.java
@@ -0,0 +1,31 @@
+public class People {
+
+        private String Login;
+        private Integer Password;
+
+        void info (){
+            System.out.println("User information:");
+            System.out.println("User login: " + Login +  "/n User Password " + Password);
+
+
+
+        }
+
+        public String getLogin() {
+            return Login;
+        }
+
+        public Integer getPassword() {
+            return Password;
+        }
+
+        public void setLogin(String login) {
+            Login = login;
+        }
+
+        public void setPassword(Integer password) {
+            this.Password = password;
+        }
+    }
+
+
